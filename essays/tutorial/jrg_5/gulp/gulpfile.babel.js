173ebc7ae2507dbc94c2f3028d54aacc0ca218e5
slashhuang <627284482@qq.com>
2017-05-29 20:23:54 +0800

    add gulp lesson

---
+++ essays/tutorial/jrg_5/gulp/gulpfile.babel.js
@@ -0,0 +1,60 @@
+import gulp from 'gulp';
+import less from 'gulp-less';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import cleanCSS from 'gulp-clean-css';
+import del from 'del';
+const paths = {
+  styles: {
+    src: 'src/styles/**/*.less',
+    dest: 'assets/styles/'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'assets/scripts/'
+  }
+};
+
+/*
+ * For small tasks you can use arrow functions and export
+ */
+const clean = () => del([ 'assets' ]);
+export { clean };
+
+/*
+ * You can still declare named functions and export them as tasks
+ */
+export function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(less())
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+export function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+export function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts));
+export { build };
+/*
+ * Export a default task
+ */
+export default build;
+watch();
